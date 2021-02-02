fa3ce797ee3a10cc91660d724b0f23a55a0529d8
Martin Hundebøll <mhu@silicom.dk>
2021-02-02 10:44:58 +0100

    pci: add Silicom Denmark vendor id
    Update pci_ids.h with the vendor id for Silicom Denmark.
    
    Signed-off-by: Martin Hundebøll <mhu@silicom.dk>

---
+++ include/linux/pci_ids.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef BACKPORT_PCI_IDS_H
+#define BACKPORT_PCI_IDS_H
+
+#include_next <linux/pci_ids.h>
+
+#ifndef PCI_VENDOR_ID_SILICOM_DENMARK
+#define PCI_VENDOR_ID_SILICOM_DENMARK	0x1c2c
+#endif
+
+#endif
