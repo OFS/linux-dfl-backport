OFS/linux-dfl

net: ethernet: intel: interact with actual 10G ethernet IP
Update driver to interact with actual 10G ethernet IP via a
regmap.  Driver supports fetching/clearing statistics registers
and enabling/disabling loopback.

Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>

drivers/net/ethernet/intel/intel_ll_10g_mac.c
@@ -15,12 +15,25 @@
 #include <linux/etherdevice.h>
 #include <linux/uaccess.h>
 
+#define MB_BASE_OFFSET		0x30
+
+#define PHY_BASE_OFF		0x2000
+#define PHY_RX_SER_LOOP_BACK	0x4e1
+
+#define ILL_10G_TX_STATS_CLR	0x1c00
+#define ILL_10G_RX_STATS_CLR	0x0c00
+
+#define ILL_10G_STATS_CLR_INT_US		1
+#define ILL_10G_STATS_CLR_INT_TIMEOUT_US	1000
+
 struct intel_ll_10g_drvdata {
 	struct net_device *netdev;
 };
 
 struct intel_ll_10g_netdata {
 	struct dfl_device *dfl_dev;
+	struct regmap *regmap;
+	struct dfl_regmap_debug *debug;
 };
 
 static int netdev_change_mtu(struct net_device *netdev, int new_mtu)
@@ -30,9 +43,22 @@ static int netdev_change_mtu(struct net_device *netdev, int new_mtu)
 	return 0;
 }
 
-static int netdev_set_features(struct net_device *dev,
+static int netdev_set_loopback(struct net_device *netdev, bool en)
+{
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
+	u32 val = en;
+
+	return(regmap_write(npriv->regmap, (PHY_BASE_OFF + PHY_RX_SER_LOOP_BACK), val));
+}
+
+static int netdev_set_features(struct net_device *netdev,
 			       netdev_features_t features)
 {
+	netdev_features_t changed = netdev->features ^ features;
+
+	if (changed & NETIF_F_LOOPBACK)
+		return netdev_set_loopback(netdev, !!(features & NETIF_F_LOOPBACK));
+
 	return 0;
 }
 
@@ -63,68 +89,68 @@ struct stat_info {
 
 static struct stat_info stats_10g[] = {
 	/* TX Statistics */
-	{STAT_INFO(0x142, "tx_frame_ok")},
-	{STAT_INFO(0x144, "tx_frame_err")},
-	{STAT_INFO(0x146, "tx_frame_crc_err")},
-	{STAT_INFO(0x148, "tx_octets_ok")},
-	{STAT_INFO(0x14a, "tx_pause_mac_ctrl_frames")},
-	{STAT_INFO(0x14c, "tx_if_err")},
-	{STAT_INFO(0x14e, "tx_unicast_frame_ok")},
-	{STAT_INFO(0x150, "tx_unicast_frame_err")},
-	{STAT_INFO(0x152, "tx_multicast_frame_ok")},
-	{STAT_INFO(0x154, "tx_multicast_frame_err")},
-	{STAT_INFO(0x156, "tx_broadcast_frame_ok")},
-	{STAT_INFO(0x158, "tx_broadcast_frame_err")},
-	{STAT_INFO(0x15a, "tx_ether_octets")},
-	{STAT_INFO(0x15c, "tx_ether_pkts")},
-	{STAT_INFO(0x15e, "tx_ether_undersize_pkts")},
-	{STAT_INFO(0x160, "tx_ether_oversize_pkts")},
-	{STAT_INFO(0x162, "tx_ether_pkts_64_octets")},
-	{STAT_INFO(0x164, "tx_ether_pkts_65_127_octets")},
-	{STAT_INFO(0x166, "tx_ether_pkts_128_255_octets")},
-	{STAT_INFO(0x168, "tx_ether_pkts_256_511_octets")},
-	{STAT_INFO(0x16a, "tx_ether_pkts_512_1023_octets")},
-	{STAT_INFO(0x16c, "tx_ether_pkts_1024_1518_octets")},
-	{STAT_INFO(0x16e, "tx_ether_pkts_1519_x_octets")},
-	/* {STAT_INFO(0x170, "tx_ether_fragments")}, */
-	/* {STAT_INFO(0x172, "tx_ether_jabbers")}, */
-	/* {STAT_INFO(0x174, "tx_ether_crc_err")}, */
-	{STAT_INFO(0x176, "tx_unicast_mac_ctrl_frames")},
-	{STAT_INFO(0x178, "tx_multicast_mac_ctrl_frames")},
-	{STAT_INFO(0x17a, "tx_broadcast_mac_ctrl_frames")},
-	{STAT_INFO(0x17c, "tx_pfc_mac_ctrl_frames")},
+	{STAT_INFO(0x1c02, "tx_frame_ok")},
+	{STAT_INFO(0x1c04, "tx_frame_err")},
+	{STAT_INFO(0x1c06, "tx_frame_crc_err")},
+	{STAT_INFO(0x1c08, "tx_octets_ok")},
+	{STAT_INFO(0x1c0a, "tx_pause_mac_ctrl_frames")},
+	{STAT_INFO(0x1c0c, "tx_if_err")},
+	{STAT_INFO(0x1c0e, "tx_unicast_frame_ok")},
+	{STAT_INFO(0x1c10, "tx_unicast_frame_err")},
+	{STAT_INFO(0x1c12, "tx_multicast_frame_ok")},
+	{STAT_INFO(0x1c14, "tx_multicast_frame_err")},
+	{STAT_INFO(0x1c16, "tx_broadcast_frame_ok")},
+	{STAT_INFO(0x1c18, "tx_broadcast_frame_err")},
+	{STAT_INFO(0x1c1a, "tx_ether_octets")},
+	{STAT_INFO(0x1c1c, "tx_ether_pkts")},
+	{STAT_INFO(0x1c1e, "tx_ether_undersize_pkts")},
+	{STAT_INFO(0x1c20, "tx_ether_oversize_pkts")},
+	{STAT_INFO(0x1c22, "tx_ether_pkts_64_octets")},
+	{STAT_INFO(0x1c24, "tx_ether_pkts_65_127_octets")},
+	{STAT_INFO(0x1c26, "tx_ether_pkts_128_255_octets")},
+	{STAT_INFO(0x1c28, "tx_ether_pkts_256_511_octets")},
+	{STAT_INFO(0x1c2a, "tx_ether_pkts_512_1023_octets")},
+	{STAT_INFO(0x1c2c, "tx_ether_pkts_1024_1518_octets")},
+	{STAT_INFO(0x1c2e, "tx_ether_pkts_1519_x_octets")},
+	{STAT_INFO(0x1c30, "tx_ether_fragments")},
+	{STAT_INFO(0x1c32, "tx_ether_jabbers")},
+	{STAT_INFO(0x1c34, "tx_ether_crc_err")},
+	{STAT_INFO(0x1c36, "tx_unicast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c38, "tx_multicast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c3a, "tx_broadcast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c3c, "tx_pfc_mac_ctrl_frames")},
 
 	/* RX Statistics */
-	{STAT_INFO(0x1c2, "rx_frame_ok")},
-	{STAT_INFO(0x1c4, "rx_frame_err")},
-	{STAT_INFO(0x1c6, "rx_frame_crc_err")},
-	{STAT_INFO(0x1c8, "rx_octets_ok")},
-	{STAT_INFO(0x1ca, "rx_pause_mac_ctrl_frames")},
-	{STAT_INFO(0x1cc, "rx_if_err")},
-	{STAT_INFO(0x1ce, "rx_unicast_frame_ok")},
-	{STAT_INFO(0x1d0, "rx_unicast_frame_err")},
-	{STAT_INFO(0x1d2, "rx_multicast_frame_ok")},
-	{STAT_INFO(0x1d4, "rx_multicast_frame_err")},
-	{STAT_INFO(0x1d6, "rx_broadcast_frame_ok")},
-	{STAT_INFO(0x1d8, "rx_broadcast_frame_err")},
-	{STAT_INFO(0x1da, "rx_ether_octets")},
-	{STAT_INFO(0x1dc, "rx_ether_pkts")},
-	{STAT_INFO(0x1de, "rx_ether_undersize_pkts")},
-	{STAT_INFO(0x1e0, "rx_ether_oversize_pkts")},
-	{STAT_INFO(0x1e2, "rx_ether_pkts_64_octets")},
-	{STAT_INFO(0x1e4, "rx_ether_pkts_65_127_octets")},
-	{STAT_INFO(0x1e6, "rx_ether_pkts_128_255_octets")},
-	{STAT_INFO(0x1e8, "rx_ether_pkts_256_511_octets")},
-	{STAT_INFO(0x1ea, "rx_ether_pkts_512_1023_octets")},
-	{STAT_INFO(0x1ec, "rx_ether_pkts_1024_1518_octets")},
-	{STAT_INFO(0x1ee, "rx_ether_pkts_1519_x_octets")},
-	{STAT_INFO(0x1f0, "rx_ether_fragments")},
-	{STAT_INFO(0x1f2, "rx_ether_jabbers")},
-	{STAT_INFO(0x1f4, "rx_ether_crc_err")},
-	{STAT_INFO(0x1f6, "rx_unicast_mac_ctrl_frames")},
-	{STAT_INFO(0x1f8, "rx_multicast_mac_ctrl_frames")},
-	{STAT_INFO(0x1fa, "rx_broadcast_mac_ctrl_frames")},
-	{STAT_INFO(0x1fc, "rx_pfc_mac_ctrl_frames")},
+	{STAT_INFO(0x0c02, "rx_frame_ok")},
+	{STAT_INFO(0x0c04, "rx_frame_err")},
+	{STAT_INFO(0x0c06, "rx_frame_crc_err")},
+	{STAT_INFO(0x0c08, "rx_octets_ok")},
+	{STAT_INFO(0x0c0a, "rx_pause_mac_ctrl_frames")},
+	{STAT_INFO(0x0c0c, "rx_if_err")},
+	{STAT_INFO(0x0c0e, "rx_unicast_frame_ok")},
+	{STAT_INFO(0x0c10, "rx_unicast_frame_err")},
+	{STAT_INFO(0x0c12, "rx_multicast_frame_ok")},
+	{STAT_INFO(0x0c14, "rx_multicast_frame_err")},
+	{STAT_INFO(0x0c16, "rx_broadcast_frame_ok")},
+	{STAT_INFO(0x0c18, "rx_broadcast_frame_err")},
+	{STAT_INFO(0x0c1a, "rx_ether_octets")},
+	{STAT_INFO(0x0c1c, "rx_ether_pkts")},
+	{STAT_INFO(0x0c1e, "rx_ether_undersize_pkts")},
+	{STAT_INFO(0x0c20, "rx_ether_oversize_pkts")},
+	{STAT_INFO(0x0c22, "rx_ether_pkts_64_octets")},
+	{STAT_INFO(0x0c24, "rx_ether_pkts_65_127_octets")},
+	{STAT_INFO(0x0c26, "rx_ether_pkts_128_255_octets")},
+	{STAT_INFO(0x0c28, "rx_ether_pkts_256_511_octets")},
+	{STAT_INFO(0x0c2a, "rx_ether_pkts_512_1023_octets")},
+	{STAT_INFO(0x0c2c, "rx_ether_pkts_1024_1518_octets")},
+	{STAT_INFO(0x0c2e, "rx_ether_pkts_1519_x_octets")},
+	{STAT_INFO(0x0c30, "rx_ether_fragments")},
+	{STAT_INFO(0x0c32, "rx_ether_jabbers")},
+	{STAT_INFO(0x0c34, "rx_ether_crc_err")},
+	{STAT_INFO(0x0c36, "rx_unicast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c38, "rx_multicast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c3a, "rx_broadcast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c3c, "rx_pfc_mac_ctrl_frames")},
 };
 
 static void ethtool_get_strings(struct net_device *netdev, u32 stringset,
@@ -157,20 +183,68 @@ static int ethtool_get_sset_count(struct net_device *netdev, int stringset)
 	}
 }
 
+static u64 read_mac_stat(struct regmap *regmap, unsigned int addr)
+{
+	u32 data_l, data_h;
+
+	regmap_read(regmap, addr, &data_l);
+	regmap_read(regmap, addr + 1, &data_h);
+
+	return data_l + ((u64)data_h << 32);
+}
+
+static int ethtool_reset(struct net_device *netdev, u32 *flags)
+{
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
+	int ret;
+	u32 val;
+
+	if (*flags | ETH_RESET_MGMT) {
+		regmap_write(npriv->regmap, ILL_10G_TX_STATS_CLR, 1);
+
+		ret = regmap_read_poll_timeout(npriv->regmap,  ILL_10G_TX_STATS_CLR,
+					       val, (!val), ILL_10G_STATS_CLR_INT_US,
+					       ILL_10G_STATS_CLR_INT_TIMEOUT_US);
+
+		if (ret) {
+			dev_err(&netdev->dev, "%s failed to clear tx stats\n", __func__);
+			return ret;
+		}
+
+		regmap_write(npriv->regmap, ILL_10G_RX_STATS_CLR, 1);
+
+		ret = regmap_read_poll_timeout(npriv->regmap,  ILL_10G_RX_STATS_CLR,
+					       val, (!val), ILL_10G_STATS_CLR_INT_US,
+					       ILL_10G_STATS_CLR_INT_TIMEOUT_US);
+
+		if (ret) {
+			dev_err(&netdev->dev, "%s failed to clear rx stats\n", __func__);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
 static void ethtool_get_stats(struct net_device *netdev,
 			      struct ethtool_stats *stats, u64 *data)
 {
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
 	unsigned int i, stats_num = ARRAY_SIZE(stats_10g);
 	struct stat_info *stat = stats_10g;
+	u32 flags = ETH_RESET_MGMT;
 
 	for (i = 0; i < stats_num; i++)
-		data[i] = stat[i].addr;
+		data[i] = read_mac_stat(npriv->regmap, stat[i].addr);
+
+	ethtool_reset(netdev, &flags);
 }
 
 static const struct ethtool_ops ethtool_ops = {
 	.get_strings = ethtool_get_strings,
 	.get_sset_count = ethtool_get_sset_count,
 	.get_ethtool_stats = ethtool_get_stats,
+	.reset = ethtool_reset,
 };
 
 static void intel_ll_10g_init_netdev(struct net_device *netdev)
@@ -189,18 +263,30 @@ static void intel_ll_10g_init_netdev(struct net_device *netdev)
 
 static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 {
+	struct device *dev = &dfl_dev->dev;
 	struct intel_ll_10g_netdata *npriv;
 	struct intel_ll_10g_drvdata *priv;
+	struct regmap *regmap;
+	void __iomem *base;
+	u32 flags;
 	int ret;
 
-	priv = devm_kzalloc(&dfl_dev->dev, sizeof(*priv), GFP_KERNEL);
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 
 	if (!priv)
 		return -ENOMEM;
 
-	dev_set_drvdata(&dfl_dev->dev, priv);
+	dev_set_drvdata(dev, priv);
 
-	dev_info(&dfl_dev->dev, "%s priv %p\n", __func__, priv);
+	base = devm_ioremap_resource(dev, &dfl_dev->mmio_res);
+
+	if (!base)
+		return -ENOMEM;
+
+	regmap = dfl_indirect_regmap_init(dev, base, MB_BASE_OFFSET);
+
+	if (!regmap)
+		return -ENOMEM;
 
 	priv->netdev = alloc_netdev(sizeof(struct intel_ll_10g_netdata),
 				    "ll_10g%d", NET_NAME_UNKNOWN,
@@ -212,8 +298,19 @@ static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 	npriv = netdev_priv(priv->netdev);
 
 	npriv->dfl_dev = dfl_dev;
+	npriv->regmap = regmap;
+	npriv->debug = dfl_regmap_debug_init(dev, regmap);
+
 
 	SET_NETDEV_DEV(priv->netdev, &dfl_dev->dev);
+
+	flags = ETH_RESET_MGMT;
+
+	ret = ethtool_reset(priv->netdev, &flags);
+
+	if (ret)
+		dev_err(&dfl_dev->dev, "failed to reset MGMT %s: %d",
+			priv->netdev->name, ret);
 
 	ret = register_netdev(priv->netdev);
 
@@ -227,8 +324,9 @@ static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 static int intel_ll_10g_mac_remove(struct dfl_device *dfl_dev)
 {
 	struct intel_ll_10g_drvdata *priv = dev_get_drvdata(&dfl_dev->dev);
+	struct intel_ll_10g_netdata *npriv = netdev_priv(priv->netdev);
 
-	dev_info(&dfl_dev->dev, "%s %p\n", __func__, priv);
+	dfl_regmap_debug_exit(npriv->debug);
 
 	unregister_netdev(priv->netdev);
 

OFS/linux-dfl-backport

drivers/net/ethernet/intel/intel_ll_10g_mac.c
@@ -15,12 +15,26 @@
 #include <linux/etherdevice.h>
 #include <linux/uaccess.h>
 
+#define CAPABILITY_OFFSET	0x08
+#define MB_BASE_OFFSET		0x28
+
+#define PHY_BASE_OFF		0x2000
+#define PHY_RX_SER_LOOP_BACK	0x4e1
+
+#define ILL_10G_TX_STATS_CLR	0x1c00
+#define ILL_10G_RX_STATS_CLR	0x0c00
+
+#define ILL_10G_STATS_CLR_INT_US		1
+#define ILL_10G_STATS_CLR_INT_TIMEOUT_US	1000
+
 struct intel_ll_10g_drvdata {
 	struct net_device *netdev;
 };
 
 struct intel_ll_10g_netdata {
 	struct dfl_device *dfl_dev;
+	struct regmap *regmap;
+	struct dfl_regmap_debug *debug;
 };
 
 static int netdev_change_mtu(struct net_device *netdev, int new_mtu)
@@ -30,9 +44,22 @@ static int netdev_change_mtu(struct net_device *netdev, int new_mtu)
 	return 0;
 }
 
-static int netdev_set_features(struct net_device *dev,
+static int netdev_set_loopback(struct net_device *netdev, bool en)
+{
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
+	u32 val = en;
+
+	return(regmap_write(npriv->regmap, (PHY_BASE_OFF + PHY_RX_SER_LOOP_BACK), val));
+}
+
+static int netdev_set_features(struct net_device *netdev,
 			       netdev_features_t features)
 {
+	netdev_features_t changed = netdev->features ^ features;
+
+	if (changed & NETIF_F_LOOPBACK)
+		return netdev_set_loopback(netdev, !!(features & NETIF_F_LOOPBACK));
+
 	return 0;
 }
 
@@ -63,68 +90,68 @@ struct stat_info {
 
 static struct stat_info stats_10g[] = {
 	/* TX Statistics */
-	{STAT_INFO(0x142, "tx_frame_ok")},
-	{STAT_INFO(0x144, "tx_frame_err")},
-	{STAT_INFO(0x146, "tx_frame_crc_err")},
-	{STAT_INFO(0x148, "tx_octets_ok")},
-	{STAT_INFO(0x14a, "tx_pause_mac_ctrl_frames")},
-	{STAT_INFO(0x14c, "tx_if_err")},
-	{STAT_INFO(0x14e, "tx_unicast_frame_ok")},
-	{STAT_INFO(0x150, "tx_unicast_frame_err")},
-	{STAT_INFO(0x152, "tx_multicast_frame_ok")},
-	{STAT_INFO(0x154, "tx_multicast_frame_err")},
-	{STAT_INFO(0x156, "tx_broadcast_frame_ok")},
-	{STAT_INFO(0x158, "tx_broadcast_frame_err")},
-	{STAT_INFO(0x15a, "tx_ether_octets")},
-	{STAT_INFO(0x15c, "tx_ether_pkts")},
-	{STAT_INFO(0x15e, "tx_ether_undersize_pkts")},
-	{STAT_INFO(0x160, "tx_ether_oversize_pkts")},
-	{STAT_INFO(0x162, "tx_ether_pkts_64_octets")},
-	{STAT_INFO(0x164, "tx_ether_pkts_65_127_octets")},
-	{STAT_INFO(0x166, "tx_ether_pkts_128_255_octets")},
-	{STAT_INFO(0x168, "tx_ether_pkts_256_511_octets")},
-	{STAT_INFO(0x16a, "tx_ether_pkts_512_1023_octets")},
-	{STAT_INFO(0x16c, "tx_ether_pkts_1024_1518_octets")},
-	{STAT_INFO(0x16e, "tx_ether_pkts_1519_x_octets")},
-	/* {STAT_INFO(0x170, "tx_ether_fragments")}, */
-	/* {STAT_INFO(0x172, "tx_ether_jabbers")}, */
-	/* {STAT_INFO(0x174, "tx_ether_crc_err")}, */
-	{STAT_INFO(0x176, "tx_unicast_mac_ctrl_frames")},
-	{STAT_INFO(0x178, "tx_multicast_mac_ctrl_frames")},
-	{STAT_INFO(0x17a, "tx_broadcast_mac_ctrl_frames")},
-	{STAT_INFO(0x17c, "tx_pfc_mac_ctrl_frames")},
+	{STAT_INFO(0x1c02, "tx_frame_ok")},
+	{STAT_INFO(0x1c04, "tx_frame_err")},
+	{STAT_INFO(0x1c06, "tx_frame_crc_err")},
+	{STAT_INFO(0x1c08, "tx_octets_ok")},
+	{STAT_INFO(0x1c0a, "tx_pause_mac_ctrl_frames")},
+	{STAT_INFO(0x1c0c, "tx_if_err")},
+	{STAT_INFO(0x1c0e, "tx_unicast_frame_ok")},
+	{STAT_INFO(0x1c10, "tx_unicast_frame_err")},
+	{STAT_INFO(0x1c12, "tx_multicast_frame_ok")},
+	{STAT_INFO(0x1c14, "tx_multicast_frame_err")},
+	{STAT_INFO(0x1c16, "tx_broadcast_frame_ok")},
+	{STAT_INFO(0x1c18, "tx_broadcast_frame_err")},
+	{STAT_INFO(0x1c1a, "tx_ether_octets")},
+	{STAT_INFO(0x1c1c, "tx_ether_pkts")},
+	{STAT_INFO(0x1c1e, "tx_ether_undersize_pkts")},
+	{STAT_INFO(0x1c20, "tx_ether_oversize_pkts")},
+	{STAT_INFO(0x1c22, "tx_ether_pkts_64_octets")},
+	{STAT_INFO(0x1c24, "tx_ether_pkts_65_127_octets")},
+	{STAT_INFO(0x1c26, "tx_ether_pkts_128_255_octets")},
+	{STAT_INFO(0x1c28, "tx_ether_pkts_256_511_octets")},
+	{STAT_INFO(0x1c2a, "tx_ether_pkts_512_1023_octets")},
+	{STAT_INFO(0x1c2c, "tx_ether_pkts_1024_1518_octets")},
+	{STAT_INFO(0x1c2e, "tx_ether_pkts_1519_x_octets")},
+	{STAT_INFO(0x1c30, "tx_ether_fragments")},
+	{STAT_INFO(0x1c32, "tx_ether_jabbers")},
+	{STAT_INFO(0x1c34, "tx_ether_crc_err")},
+	{STAT_INFO(0x1c36, "tx_unicast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c38, "tx_multicast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c3a, "tx_broadcast_mac_ctrl_frames")},
+	{STAT_INFO(0x1c3c, "tx_pfc_mac_ctrl_frames")},
 
 	/* RX Statistics */
-	{STAT_INFO(0x1c2, "rx_frame_ok")},
-	{STAT_INFO(0x1c4, "rx_frame_err")},
-	{STAT_INFO(0x1c6, "rx_frame_crc_err")},
-	{STAT_INFO(0x1c8, "rx_octets_ok")},
-	{STAT_INFO(0x1ca, "rx_pause_mac_ctrl_frames")},
-	{STAT_INFO(0x1cc, "rx_if_err")},
-	{STAT_INFO(0x1ce, "rx_unicast_frame_ok")},
-	{STAT_INFO(0x1d0, "rx_unicast_frame_err")},
-	{STAT_INFO(0x1d2, "rx_multicast_frame_ok")},
-	{STAT_INFO(0x1d4, "rx_multicast_frame_err")},
-	{STAT_INFO(0x1d6, "rx_broadcast_frame_ok")},
-	{STAT_INFO(0x1d8, "rx_broadcast_frame_err")},
-	{STAT_INFO(0x1da, "rx_ether_octets")},
-	{STAT_INFO(0x1dc, "rx_ether_pkts")},
-	{STAT_INFO(0x1de, "rx_ether_undersize_pkts")},
-	{STAT_INFO(0x1e0, "rx_ether_oversize_pkts")},
-	{STAT_INFO(0x1e2, "rx_ether_pkts_64_octets")},
-	{STAT_INFO(0x1e4, "rx_ether_pkts_65_127_octets")},
-	{STAT_INFO(0x1e6, "rx_ether_pkts_128_255_octets")},
-	{STAT_INFO(0x1e8, "rx_ether_pkts_256_511_octets")},
-	{STAT_INFO(0x1ea, "rx_ether_pkts_512_1023_octets")},
-	{STAT_INFO(0x1ec, "rx_ether_pkts_1024_1518_octets")},
-	{STAT_INFO(0x1ee, "rx_ether_pkts_1519_x_octets")},
-	{STAT_INFO(0x1f0, "rx_ether_fragments")},
-	{STAT_INFO(0x1f2, "rx_ether_jabbers")},
-	{STAT_INFO(0x1f4, "rx_ether_crc_err")},
-	{STAT_INFO(0x1f6, "rx_unicast_mac_ctrl_frames")},
-	{STAT_INFO(0x1f8, "rx_multicast_mac_ctrl_frames")},
-	{STAT_INFO(0x1fa, "rx_broadcast_mac_ctrl_frames")},
-	{STAT_INFO(0x1fc, "rx_pfc_mac_ctrl_frames")},
+	{STAT_INFO(0x0c02, "rx_frame_ok")},
+	{STAT_INFO(0x0c04, "rx_frame_err")},
+	{STAT_INFO(0x0c06, "rx_frame_crc_err")},
+	{STAT_INFO(0x0c08, "rx_octets_ok")},
+	{STAT_INFO(0x0c0a, "rx_pause_mac_ctrl_frames")},
+	{STAT_INFO(0x0c0c, "rx_if_err")},
+	{STAT_INFO(0x0c0e, "rx_unicast_frame_ok")},
+	{STAT_INFO(0x0c10, "rx_unicast_frame_err")},
+	{STAT_INFO(0x0c12, "rx_multicast_frame_ok")},
+	{STAT_INFO(0x0c14, "rx_multicast_frame_err")},
+	{STAT_INFO(0x0c16, "rx_broadcast_frame_ok")},
+	{STAT_INFO(0x0c18, "rx_broadcast_frame_err")},
+	{STAT_INFO(0x0c1a, "rx_ether_octets")},
+	{STAT_INFO(0x0c1c, "rx_ether_pkts")},
+	{STAT_INFO(0x0c1e, "rx_ether_undersize_pkts")},
+	{STAT_INFO(0x0c20, "rx_ether_oversize_pkts")},
+	{STAT_INFO(0x0c22, "rx_ether_pkts_64_octets")},
+	{STAT_INFO(0x0c24, "rx_ether_pkts_65_127_octets")},
+	{STAT_INFO(0x0c26, "rx_ether_pkts_128_255_octets")},
+	{STAT_INFO(0x0c28, "rx_ether_pkts_256_511_octets")},
+	{STAT_INFO(0x0c2a, "rx_ether_pkts_512_1023_octets")},
+	{STAT_INFO(0x0c2c, "rx_ether_pkts_1024_1518_octets")},
+	{STAT_INFO(0x0c2e, "rx_ether_pkts_1519_x_octets")},
+	{STAT_INFO(0x0c30, "rx_ether_fragments")},
+	{STAT_INFO(0x0c32, "rx_ether_jabbers")},
+	{STAT_INFO(0x0c34, "rx_ether_crc_err")},
+	{STAT_INFO(0x0c36, "rx_unicast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c38, "rx_multicast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c3a, "rx_broadcast_mac_ctrl_frames")},
+	{STAT_INFO(0x0c3c, "rx_pfc_mac_ctrl_frames")},
 };
 
 static void ethtool_get_strings(struct net_device *netdev, u32 stringset,
@@ -157,20 +184,68 @@ static int ethtool_get_sset_count(struct net_device *netdev, int stringset)
 	}
 }
 
+static u64 read_mac_stat(struct regmap *regmap, unsigned int addr)
+{
+	u32 data_l, data_h;
+
+	regmap_read(regmap, addr, &data_l);
+	regmap_read(regmap, addr + 1, &data_h);
+
+	return data_l + ((u64)data_h << 32);
+}
+
+static int ethtool_reset(struct net_device *netdev, u32 *flags)
+{
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
+	int ret;
+	u32 val;
+
+	if (*flags | ETH_RESET_MGMT) {
+		regmap_write(npriv->regmap, ILL_10G_TX_STATS_CLR, 1);
+
+		ret = regmap_read_poll_timeout(npriv->regmap,  ILL_10G_TX_STATS_CLR,
+					       val, (!val), ILL_10G_STATS_CLR_INT_US,
+					       ILL_10G_STATS_CLR_INT_TIMEOUT_US);
+
+		if (ret) {
+			dev_err(&netdev->dev, "%s failed to clear tx stats\n", __func__);
+			return ret;
+		}
+
+		regmap_write(npriv->regmap, ILL_10G_RX_STATS_CLR, 1);
+
+		ret = regmap_read_poll_timeout(npriv->regmap,  ILL_10G_RX_STATS_CLR,
+					       val, (!val), ILL_10G_STATS_CLR_INT_US,
+					       ILL_10G_STATS_CLR_INT_TIMEOUT_US);
+
+		if (ret) {
+			dev_err(&netdev->dev, "%s failed to clear rx stats\n", __func__);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
 static void ethtool_get_stats(struct net_device *netdev,
 			      struct ethtool_stats *stats, u64 *data)
 {
+	struct intel_ll_10g_netdata *npriv = netdev_priv(netdev);
 	unsigned int i, stats_num = ARRAY_SIZE(stats_10g);
 	struct stat_info *stat = stats_10g;
+	u32 flags = ETH_RESET_MGMT;
 
 	for (i = 0; i < stats_num; i++)
-		data[i] = stat[i].addr;
+		data[i] = read_mac_stat(npriv->regmap, stat[i].addr);
+
+	ethtool_reset(netdev, &flags);
 }
 
 static const struct ethtool_ops ethtool_ops = {
 	.get_strings = ethtool_get_strings,
 	.get_sset_count = ethtool_get_sset_count,
 	.get_ethtool_stats = ethtool_get_stats,
+	.reset = ethtool_reset,
 };
 
 static void intel_ll_10g_init_netdev(struct net_device *netdev)
@@ -189,18 +264,35 @@ static void intel_ll_10g_init_netdev(struct net_device *netdev)
 
 static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 {
+	struct device *dev = &dfl_dev->dev;
 	struct intel_ll_10g_netdata *npriv;
 	struct intel_ll_10g_drvdata *priv;
+	struct regmap *regmap;
+	void __iomem *base;
+	u32 flags;
+	u64 val;
 	int ret;
 
-	priv = devm_kzalloc(&dfl_dev->dev, sizeof(*priv), GFP_KERNEL);
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 
 	if (!priv)
 		return -ENOMEM;
 
-	dev_set_drvdata(&dfl_dev->dev, priv);
+	dev_set_drvdata(dev, priv);
 
-	dev_info(&dfl_dev->dev, "%s priv %p\n", __func__, priv);
+	base = devm_ioremap_resource(dev, &dfl_dev->mmio_res);
+
+	if (!base)
+		return -ENOMEM;
+
+	val = readq(base + CAPABILITY_OFFSET);
+
+	dev_info(dev, "%s capability register 0x%llx\n", __func__, val);
+
+	regmap = dfl_indirect_regmap_init(dev, base, MB_BASE_OFFSET);
+
+	if (!regmap)
+		return -ENOMEM;
 
 	priv->netdev = alloc_netdev(sizeof(struct intel_ll_10g_netdata),
 				    "ll_10g%d", NET_NAME_UNKNOWN,
@@ -212,8 +304,19 @@ static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 	npriv = netdev_priv(priv->netdev);
 
 	npriv->dfl_dev = dfl_dev;
+	npriv->regmap = regmap;
+	npriv->debug = dfl_regmap_debug_init(dev, regmap);
+
 
 	SET_NETDEV_DEV(priv->netdev, &dfl_dev->dev);
+
+	flags = ETH_RESET_MGMT;
+
+	ret = ethtool_reset(priv->netdev, &flags);
+
+	if (ret)
+		dev_err(&dfl_dev->dev, "failed to reset MGMT %s: %d",
+			priv->netdev->name, ret);
 
 	ret = register_netdev(priv->netdev);
 
@@ -227,8 +330,9 @@ static int intel_ll_10g_mac_probe(struct dfl_device *dfl_dev)
 static int intel_ll_10g_mac_remove(struct dfl_device *dfl_dev)
 {
 	struct intel_ll_10g_drvdata *priv = dev_get_drvdata(&dfl_dev->dev);
+	struct intel_ll_10g_netdata *npriv = netdev_priv(priv->netdev);
 
-	dev_info(&dfl_dev->dev, "%s %p\n", __func__, priv);
+	dfl_regmap_debug_exit(npriv->debug);
 
 	unregister_netdev(priv->netdev);
 