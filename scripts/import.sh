851936f21ab794e9da040dfe312e98865ef0e281
Martin Hundebøll <mhu@silicom.dk>
2021-03-04 19:59:16 +0100

    scripts: add manifest and import script for fpga-ofs-dev-5.10.0-lts
    The import script takes a few arguments:
    
      ./scripts/import.sh <manifest> <mainline-rev> <dfl-range>
    
    For example, one can create a base branch based on v5.10.19 with dfl
    commits from the fpga-ofs-dev-5.10.0-lts branch by running:
    
      ./scripts/import.sh scripts/manifest.v5.10 v5.10.19 v5.10.4..fpga-ofs-dev-5.10-lts
    
    The script is made to ignore dfl commits containing DEBUG or REVERTME in
    the shortlog line, and it ignores merges and secondary parents (i.e.
    merges of stable branches).

---
+++ scripts/import.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+manifest=$1
+revision=$2
+range=$3
+
+remote() {
+  name=$1
+  url=$2
+
+  if ! git remote -v | grep -qE ^$name; then
+    git remote add --fetch $name $url
+  fi
+}
+
+import() {
+  git restore --pathspec-from-file $manifest --source $revision
+  git add .
+  git commit -m "Import from mainline $revision"
+}
+
+apply() {
+  git rev-list --reverse --first-parent --no-merges --invert-grep --grep REVERTME --grep DEBUG $range | \
+    git cherry-pick --strategy recursive --strategy-option no-renames --stdin
+}
+
+remote mainline git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+remote stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+remote dfl https://github.com/OPAE/linux-dfl.git
+
+import
+apply
