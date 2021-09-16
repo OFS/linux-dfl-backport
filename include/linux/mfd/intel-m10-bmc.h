OFS/linux-dfl

mfd: intel-m10-bmc: Create BMC log sub-driver
The PMCI MAX10 BMC contains high and low timestamp registers that are
to be periodically written by the host driver to facilitate BMC error
logs. Add a kernel worker thread at BMC initialization time to update
the BMC timestamp registers at a frequency of once per minute.

Signed-off-by: Russ Weight <russell.h.weight@intel.com>

include/linux/mfd/intel-m10-bmc.h
@@ -178,6 +178,9 @@ enum m10bmc_type {
 #define PMCI_M10BMC_MAC_LOW    0x20
 #define PMCI_M10BMC_MAC_HIGH    0x24
 
+#define PMCI_M10BMC_TIME_LOW	0x60
+#define PMCI_M10BMC_TIME_HIGH	0x60
+
 #define PMCI_M10BMC_FLASH_CTRL 0x1d0
 #define FLASH_MUX_SELECTION GENMASK(2, 0)
 #define FLASH_MUX_IDLE 0

OFS/linux-dfl-backport

include/linux/mfd/intel-m10-bmc.h
@@ -181,6 +181,9 @@ enum m10bmc_type {
 #define M10BMC_PMCI_MAC_LOW    0x20
 #define M10BMC_PMCI_MAC_HIGH    0x24
 
+#define M10BMC_PMCI_TIME_LOW	0x60
+#define M10BMC_PMCI_TIME_HIGH	0x60
+
 #define M10BMC_PMCI_FLASH_CTRL 0x1d0
 #define FLASH_MUX_SELECTION GENMASK(2, 0)
 #define FLASH_MUX_IDLE 0