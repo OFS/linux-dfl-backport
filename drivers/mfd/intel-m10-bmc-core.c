OFS/linux-dfl

fpga: m10bmc-sec: fix the different offset of doorbell register
The offset of doorbell register in max10 is different between
N3000/D5005 Card and PMCI-base FPGA Card. Introducing
macro definition doorbell_reg() to fix it.

Signed-off-by: Tianfei zhang <tianfei.zhang@intel.com>

drivers/mfd/intel-m10-bmc-core.c
@@ -26,6 +26,8 @@ static const struct m10bmc_csr pmci_m10bmc_csr = {
 	.fw_version = PMCI_NIOS2_FW_VERSION,
 	.mac_low = PMCI_M10BMC_MAC_LOW,
 	.mac_high = PMCI_M10BMC_MAC_HIGH,
+	.doorbell = PMCI_M10BMC_DOORBELL,
+	.auth_result = PMCI_M10BMC_AUTH_RESULT,
 };
 
 static const struct m10bmc_csr spi_m10bmc_csr = {
@@ -34,6 +36,8 @@ static const struct m10bmc_csr spi_m10bmc_csr = {
 	.fw_version = NIOS2_FW_VERSION,
 	.mac_low = M10BMC_MAC_LOW,
 	.mac_high = M10BMC_MAC_HIGH,
+	.doorbell = M10BMC_DOORBELL,
+	.auth_result = M10BMC_AUTH_RESULT,
 };
 
 static struct mfd_cell m10bmc_bmc_subdevs[] = {

include/linux/mfd/intel-m10-bmc.h
@@ -189,8 +189,15 @@ enum m10bmc_type {
 #define FLASH_NIOS_REQUEST BIT(4)
 #define FLASH_HOST_REQUEST BIT(5)
 
+#define PMCI_M10BMC_DOORBELL 0x1c0
+#define PMCI_M10BMC_AUTH_RESULT 0x1c4
+
 #define M10_FLASH_INT_US       1
 #define M10_FLASH_TIMEOUT_US   10000
+
+#define m10bmc_base(m10bmc) ((m10bmc)->csr->base)
+#define doorbell_reg(m10bmc) ((m10bmc)->csr->doorbell)
+#define auth_result_reg(m10bmc) ((m10bmc)->csr->auth_result)
 
 enum m10bmc_fw_state {
 	M10BMC_FW_STATE_NORMAL,
@@ -206,6 +213,8 @@ struct m10bmc_csr {
 	unsigned int fw_version;
 	unsigned int mac_low;
 	unsigned int mac_high;
+	unsigned int doorbell;
+	unsigned int auth_result;
 };
 
 /**

OFS/linux-dfl-backport

drivers/mfd/intel-m10-bmc-core.c
@@ -17,6 +17,8 @@ static const struct m10bmc_csr m10bmc_pmci_csr = {
 	.fw_version = NIOS2_PMCI_FW_VERSION,
 	.mac_low = M10BMC_PMCI_MAC_LOW,
 	.mac_high = M10BMC_PMCI_MAC_HIGH,
+	.doorbell = M10BMC_PMCI_DOORBELL,
+	.auth_result = M10BMC_PMCI_AUTH_RESULT,
 	.bmc_prog_addr = PMCI_BMC_PROG_ADDR,
 	.bmc_reh_addr = PMCI_BMC_REH_ADDR,
 	.bmc_magic = PMCI_BMC_PROG_MAGIC,
@@ -35,6 +37,8 @@ static const struct m10bmc_csr m10bmc_spi_csr = {
 	.fw_version = NIOS2_FW_VERSION,
 	.mac_low = M10BMC_MAC_LOW,
 	.mac_high = M10BMC_MAC_HIGH,
+	.doorbell = M10BMC_DOORBELL,
+	.auth_result = M10BMC_AUTH_RESULT,
 	.bmc_prog_addr = BMC_PROG_ADDR,
 	.bmc_reh_addr = BMC_REH_ADDR,
 	.bmc_magic = BMC_PROG_MAGIC,

include/linux/mfd/intel-m10-bmc.h
@@ -192,6 +192,9 @@ enum m10bmc_type {
 #define FLASH_NIOS_REQUEST BIT(4)
 #define FLASH_HOST_REQUEST BIT(5)
 
+#define M10BMC_PMCI_DOORBELL 0x1c0
+#define M10BMC_PMCI_AUTH_RESULT 0x1c4
+
 #define M10_FLASH_INT_US       1
 #define M10_FLASH_TIMEOUT_US   10000
 
@@ -209,6 +212,10 @@ enum m10bmc_type {
 #define PMCI_PR_PROG_MAGIC 0x5250
 
 #define PMCI_STAGING_FLASH_COUNT 0x7ff5000
+
+#define m10bmc_base(m10bmc) ((m10bmc)->csr->base)
+#define doorbell_reg(m10bmc) ((m10bmc)->csr->doorbell)
+#define auth_result_reg(m10bmc) ((m10bmc)->csr->auth_result)
 
 #define bmc_prog_addr(m10bmc) ((m10bmc)->csr->bmc_prog_addr)
 #define bmc_reh_addr(m10bmc) ((m10bmc)->csr->bmc_reh_addr)
@@ -235,6 +242,8 @@ struct m10bmc_csr {
 	unsigned int fw_version;
 	unsigned int mac_low;
 	unsigned int mac_high;
+	unsigned int doorbell;
+	unsigned int auth_result;
 	unsigned int bmc_prog_addr;
 	unsigned int bmc_reh_addr;
 	unsigned int bmc_magic;