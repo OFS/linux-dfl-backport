OFS/linux-dfl

fpga: dfl: fix VF creation when ports have no local BAR space
When a port is not connected to the same PCIe endpoint as
the FME, the port does not need to be released before being
virtualized.  Fix VF creation code to handle this new use
case.

Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>

drivers/fpga/dfl.c
@@ -1694,17 +1694,24 @@ EXPORT_SYMBOL_GPL(dfl_fpga_cdev_config_ports_pf);
 int dfl_fpga_cdev_config_ports_vf(struct dfl_fpga_cdev *cdev, int num_vfs)
 {
 	struct dfl_feature_dev_data *fdata;
-	int ret = 0;
+	int ret = 0, port_count = 0;
 
 	mutex_lock(&cdev->lock);
 	if (list_empty(&cdev->port_dev_list))
 		goto done;
+
+	list_for_each_entry(fdata, &cdev->port_dev_list, node) {
+		if (fdata->dev)
+			continue;
+		port_count++;
+	}
+
 	/*
 	 * can't turn multiple ports into 1 VF device, only 1 port for 1 VF
 	 * device, so if released port number doesn't match VF device number,
 	 * then reject the request with -EINVAL error code.
 	 */
-	if (cdev->released_port_num != num_vfs) {
+	if (port_count && (cdev->released_port_num != num_vfs)) {
 		ret = -EINVAL;
 		goto done;
 	}

OFS/linux-dfl-backport

drivers/fpga/dfl.c
@@ -1701,15 +1701,22 @@ EXPORT_SYMBOL_GPL(dfl_fpga_cdev_config_ports_pf);
 int dfl_fpga_cdev_config_ports_vf(struct dfl_fpga_cdev *cdev, int num_vfs)
 {
 	struct dfl_feature_platform_data *pdata;
-	int ret = 0;
+	int ret = 0, port_count = 0;
 
 	mutex_lock(&cdev->lock);
+
+	list_for_each_entry(pdata, &cdev->port_dev_list, node) {
+		if (pdata->dev)
+			continue;
+		port_count++;
+	}
+
 	/*
 	 * can't turn multiple ports into 1 VF device, only 1 port for 1 VF
 	 * device, so if released port number doesn't match VF device number,
 	 * then reject the request with -EINVAL error code.
 	 */
-	if (cdev->released_port_num != num_vfs) {
+	if (port_count && (cdev->released_port_num != num_vfs)) {
 		ret = -EINVAL;
 		goto done;
 	}