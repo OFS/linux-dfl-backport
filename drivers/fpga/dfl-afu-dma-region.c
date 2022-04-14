88c8fb923a062aaeaa1648cd01fc7148566d1ee6
Russ Weight <russell.h.weight@intel.com>
2022-04-14 11:00:23 -0700

    fpga: dfl: Change pdata->lock comments to fdata->lock
    Thee are several comments that reference pdata->lock. These are now
    changed to fdata->lock.
    
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>

--- drivers/fpga/dfl-afu-dma-region.c
+++ drivers/fpga/dfl-afu-dma-region.c
@@ -255,7 +255,7 @@ static bool dma_region_check_iova(struct dfl_afu_dma_region *region,
  *
  * Return 0 for success, -EEXIST if dma region has already been added.
  *
- * Needs to be called with pdata->lock heold.
+ * Needs to be called with fdata->lock held.
  */
 static int afu_dma_region_add(struct dfl_feature_dev_data *fdata,
 			      struct dfl_afu_dma_region *region)
@@ -298,7 +298,7 @@ static int afu_dma_region_add(struct dfl_feature_dev_data *fdata,
  * @fdata: feature dev data
  * @region: dma region to be removed
  *
- * Needs to be called with pdata->lock heold.
+ * Needs to be called with fdata->lock held.
  */
 static void afu_dma_region_remove(struct dfl_feature_dev_data *fdata,
 				  struct dfl_afu_dma_region *region)
@@ -317,7 +317,7 @@ static void afu_dma_region_remove(struct dfl_feature_dev_data *fdata,
  * afu_dma_region_destroy - destroy all regions in rbtree
  * @fdata: feature dev data
  *
- * Needs to be called with pdata->lock heold.
+ * Needs to be called with fdata->lock held.
  */
 void afu_dma_region_destroy(struct dfl_feature_dev_data *fdata)
 {
@@ -358,7 +358,7 @@ void afu_dma_region_destroy(struct dfl_feature_dev_data *fdata)
  *   [@iova, @iova+size)
  * If nothing is matched returns NULL.
  *
- * Needs to be called with pdata->lock held.
+ * Needs to be called with fdata->lock held.
  */
 struct dfl_afu_dma_region *
 afu_dma_region_find(struct dfl_feature_dev_data *fdata, u64 iova, u64 size)
@@ -398,7 +398,7 @@ afu_dma_region_find(struct dfl_feature_dev_data *fdata, u64 iova, u64 size)
  * @fdata: feature dev data
  * @iova: address of the dma region
  *
- * Needs to be called with pdata->lock held.
+ * Needs to be called with fdata->lock held.
  */
 static struct dfl_afu_dma_region *
 afu_dma_region_find_iova(struct dfl_feature_dev_data *fdata, u64 iova)
