4aad3278d9e5efa91838885d0e7bbb0a0ce34ce2
Russ Weight <russell.h.weight@intel.com>
2022-08-15 13:53:54 -0600

    backport: Adapt image-load for latest secure update driver
    The secure update driver has been accepted upstream and uses the new
    firmware-upload functionality of the firmware loader. Unfortunately, the
    firmware-upload functionality lives in the kernel proper, which creates
    a problem for out-of-tree drivers: if the base kernel does not include
    the firmware-upload functionality, then the out-of-tree driver must carry
    it's own solution. To handle this case, the image-load driver is being
    modified with two goals in mind:
    
    (1) Minimize changes to the secure-update driver
    (2) Require no changes to user-space code
    
    In order to accomplish these goals, a significant rewrite of the FPGA
    image load driver is required. As much as possible, code from the
    firmware-loader implementation is used to simplify future maintenance
    of the driver.
    
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>

--- drivers/fpga/fpga-image-load.c
+++ drivers/fpga/fpga-image-load.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2019-2021 Intel Corporation, Inc.
  */
 
+#include <linux/cdev.h>
 #include <linux/delay.h>
 #include <linux/fpga/fpga-image-load.h>
 #include <linux/fs.h>
@@ -22,111 +23,147 @@ static dev_t fpga_image_devt;
 
 #define to_image_load(d) container_of(d, struct fpga_image_load, dev)
 
-static void fpga_image_update_progress(struct fpga_image_load *imgld,
-				       u32 new_progress)
+/**
+ * enum fw_upload_prog - firmware upload progress codes
+ * @FW_UPLOAD_PROG_IDLE: there is no firmware upload in progress
+ * @FW_UPLOAD_PROG_RECEIVING: worker thread is receiving firmware data
+ * @FW_UPLOAD_PROG_PREPARING: target device is preparing for firmware upload
+ * @FW_UPLOAD_PROG_TRANSFERRING: data is being copied to the device
+ * @FW_UPLOAD_PROG_PROGRAMMING: device is performing the firmware update
+ * @FW_UPLOAD_PROG_MAX: Maximum progress code marker
+ */
+enum fw_upload_prog {
+	FW_UPLOAD_PROG_IDLE,
+	FW_UPLOAD_PROG_RECEIVING,
+	FW_UPLOAD_PROG_PREPARING,
+	FW_UPLOAD_PROG_TRANSFERRING,
+	FW_UPLOAD_PROG_PROGRAMMING,
+	FW_UPLOAD_PROG_MAX
+};
+
+struct fw_upload_priv {
+	struct fw_upload *fw_upload;
+	struct module *module;
+	const char *name;
+	const struct fw_upload_ops *ops;
+	struct mutex lock;		  /* protect data structure contents */
+	struct work_struct work;
+	const u8 *data;			  /* pointer to update data */
+	u32 remaining_size;		  /* size remaining to transfer */
+	enum fw_upload_prog progress;
+	enum fw_upload_prog err_progress; /* progress at time of failure */
+	enum fw_upload_err err_code;	  /* security manager error code */
+	struct eventfd_ctx *finished;
+};
+
+struct fpga_image_load {
+	struct device dev;
+	struct cdev cdev;
+	atomic_t opened;
+	void *fw_upload_priv;
+};
+
+static void fw_upload_update_progress(struct fw_upload_priv *fwlp,
+				      enum fw_upload_prog new_progress)
 {
-	mutex_lock(&imgld->lock);
-	imgld->progress = new_progress;
-	mutex_unlock(&imgld->lock);
+	mutex_lock(&fwlp->lock);
+	fwlp->progress = new_progress;
+	mutex_unlock(&fwlp->lock);
 }
 
-static void fpga_image_set_error(struct fpga_image_load *imgld, u32 err_code)
+static void fw_upload_set_error(struct fw_upload_priv *fwlp,
+				enum fw_upload_err err_code)
 {
-	mutex_lock(&imgld->lock);
-	imgld->err_progress = imgld->progress;
-	imgld->err_code = err_code;
-	mutex_unlock(&imgld->lock);
+	mutex_lock(&fwlp->lock);
+	fwlp->err_progress = fwlp->progress;
+	fwlp->err_code = err_code;
+	mutex_unlock(&fwlp->lock);
 }
 
-static void fpga_image_prog_complete(struct fpga_image_load *imgld)
+static void fw_upload_prog_complete(struct fw_upload_priv *fwlp)
 {
-	mutex_lock(&imgld->lock);
-	imgld->progress = FPGA_IMAGE_PROG_IDLE;
-	eventfd_signal(imgld->finished, 1);
-	mutex_unlock(&imgld->lock);
+	mutex_lock(&fwlp->lock);
+	fwlp->progress = FW_UPLOAD_PROG_IDLE;
+	eventfd_signal(fwlp->finished, 1);
+	mutex_unlock(&fwlp->lock);
 }
 
-static void fpga_image_do_load(struct work_struct *work)
+static void fw_upload_main(struct work_struct *work)
 {
 	struct fpga_image_load *imgld;
-	s32 ret, offset = 0;
+	struct fw_upload_priv *fwlp;
+	u32 written = 0, offset = 0;
+	enum fw_upload_err ret;
+	struct device *dev;
+	struct fw_upload *fwl;
 
-	imgld = container_of(work, struct fpga_image_load, work);
+	fwlp = container_of(work, struct fw_upload_priv, work);
+	fwl = fwlp->fw_upload;
+	imgld = (struct fpga_image_load *)fwl->priv;
+	dev = &imgld->dev;
 
-	if (imgld->driver_unload) {
-		fpga_image_set_error(imgld, FPGA_IMAGE_ERR_CANCELED);
-		goto idle_exit;
-	}
-
-	get_device(&imgld->dev);
-	if (!try_module_get(imgld->dev.parent->driver->owner)) {
-		fpga_image_set_error(imgld, FPGA_IMAGE_ERR_BUSY);
+	fw_upload_update_progress(fwlp, FW_UPLOAD_PROG_PREPARING);
+	ret = fwlp->ops->prepare(fwl, fwlp->data, fwlp->remaining_size);
+	if (ret != FW_UPLOAD_ERR_NONE) {
+		fw_upload_set_error(fwlp, ret);
 		goto putdev_exit;
 	}
 
-	fpga_image_update_progress(imgld, FPGA_IMAGE_PROG_PREPARING);
-	ret = imgld->ops->prepare(imgld, imgld->data, imgld->remaining_size);
-	if (ret) {
-		fpga_image_set_error(imgld, ret);
-		goto modput_exit;
-	}
-
-	fpga_image_update_progress(imgld, FPGA_IMAGE_PROG_WRITING);
-	while (imgld->remaining_size) {
-		ret = imgld->ops->write(imgld, imgld->data, offset,
-					imgld->remaining_size);
-		if (ret <= 0) {
-			if (!ret) {
-				dev_warn(&imgld->dev,
-					 "write-op wrote zero data\n");
-				ret = -FPGA_IMAGE_ERR_RW_ERROR;
+	fw_upload_update_progress(fwlp, FW_UPLOAD_PROG_TRANSFERRING);
+	while (fwlp->remaining_size) {
+		ret = fwlp->ops->write(fwl, fwlp->data, offset,
+					fwlp->remaining_size, &written);
+		if (ret != FW_UPLOAD_ERR_NONE || !written) {
+			if (ret == FW_UPLOAD_ERR_NONE) {
+				dev_warn(dev, "write-op wrote zero data\n");
+				ret = FW_UPLOAD_ERR_RW_ERROR;
 			}
-			fpga_image_set_error(imgld, -ret);
+			fw_upload_set_error(fwlp, ret);
 			goto done;
 		}
 
-		imgld->remaining_size -= ret;
-		offset += ret;
+		fwlp->remaining_size -= written;
+		offset += written;
 	}
 
-	fpga_image_update_progress(imgld, FPGA_IMAGE_PROG_PROGRAMMING);
-	ret = imgld->ops->poll_complete(imgld);
-	if (ret)
-		fpga_image_set_error(imgld, ret);
+	fw_upload_update_progress(fwlp, FW_UPLOAD_PROG_PROGRAMMING);
+	ret = fwlp->ops->poll_complete(fwl);
+	if (ret != FW_UPLOAD_ERR_NONE)
+		fw_upload_set_error(fwlp, ret);
 
 done:
-	if (imgld->ops->cleanup)
-		imgld->ops->cleanup(imgld);
-
-modput_exit:
-	module_put(imgld->dev.parent->driver->owner);
+	if (fwlp->ops->cleanup)
+		fwlp->ops->cleanup(fwl);
 
 putdev_exit:
-	put_device(&imgld->dev);
+	put_device(dev->parent);
 
-idle_exit:
 	/*
-	 * Note: imgld->remaining_size is left unmodified here to provide
+	 * Note: fwlp->remaining_size is left unmodified here to provide
 	 * additional information on errors. It will be reinitialized when
-	 * the next image load begins.
+	 * the next firmware upload begins.
 	 */
-	vfree(imgld->data);
-	imgld->data = NULL;
-	fpga_image_prog_complete(imgld);
-	eventfd_ctx_put(imgld->finished);
-	imgld->finished = NULL;
+	vfree(fwlp->data);
+	fwlp->data = NULL;
+	fw_upload_prog_complete(fwlp);
+	eventfd_ctx_put(fwlp->finished);
+	fwlp->finished = NULL;
 }
 
-static int fpga_image_load_ioctl_write(struct fpga_image_load *imgld,
+static int fpga_image_load_ioctl_write(struct fw_upload_priv *fwlp,
 				       unsigned long arg)
 {
+	struct fpga_image_load *imgld;
 	struct fpga_image_write wb;
+	struct fw_upload *fwl;
 	unsigned long minsz;
+	struct device *dev;
 	int ret;
 	u8 *buf;
 
-	if (imgld->driver_unload || imgld->progress != FPGA_IMAGE_PROG_IDLE)
-		return -EBUSY;
+	fwl = fwlp->fw_upload;
+	imgld = (struct fpga_image_load *)fwl->priv;
+	dev = &imgld->dev;
 
 	minsz = offsetofend(struct fpga_image_write, buf);
 	if (copy_from_user(&wb, (void __user *)arg, minsz))
@@ -150,18 +187,21 @@ static int fpga_image_load_ioctl_write(struct fpga_image_load *imgld,
 		goto exit_free;
 	}
 
-	imgld->finished = eventfd_ctx_fdget(wb.evtfd);
-	if (IS_ERR(imgld->finished)) {
-		ret = PTR_ERR(imgld->finished);
-		imgld->finished = NULL;
+	fwlp->finished = eventfd_ctx_fdget(wb.evtfd);
+	if (IS_ERR(fwlp->finished)) {
+		ret = PTR_ERR(fwlp->finished);
+		fwlp->finished = NULL;
 		goto exit_free;
 	}
 
-	imgld->data = buf;
-	imgld->remaining_size = wb.size;
-	imgld->err_code = 0;
-	imgld->progress = FPGA_IMAGE_PROG_STARTING;
-	queue_work(system_long_wq, &imgld->work);
+	get_device(dev->parent); /* released in fw_upload_main */
+
+	fwlp->progress = FW_UPLOAD_PROG_RECEIVING;
+	fwlp->err_code = 0;
+	fwlp->remaining_size = wb.size;
+	fwlp->data = buf;
+
+	queue_work(system_long_wq, &fwlp->work);
 	return 0;
 
 exit_free:
@@ -169,16 +209,69 @@ exit_free:
 	return ret;
 }
 
-static int fpga_image_load_ioctl_status(struct fpga_image_load *imgld,
+static const int fw_upload_prog_code[] = {
+	[FW_UPLOAD_PROG_IDLE]	      = FPGA_IMAGE_PROG_IDLE,
+	[FW_UPLOAD_PROG_RECEIVING]    = FPGA_IMAGE_PROG_STARTING,
+	[FW_UPLOAD_PROG_PREPARING]    = FPGA_IMAGE_PROG_PREPARING,
+	[FW_UPLOAD_PROG_TRANSFERRING] = FPGA_IMAGE_PROG_WRITING,
+	[FW_UPLOAD_PROG_PROGRAMMING]  = FPGA_IMAGE_PROG_PROGRAMMING,
+	[FW_UPLOAD_PROG_MAX]	      = FPGA_IMAGE_PROG_MAX
+};
+
+static const int fw_upload_err_code[] = {
+	[FW_UPLOAD_ERR_NONE]	     = 0,
+	[FW_UPLOAD_ERR_HW_ERROR]     = FPGA_IMAGE_ERR_HW_ERROR,
+	[FW_UPLOAD_ERR_TIMEOUT]	     = FPGA_IMAGE_ERR_TIMEOUT,
+	[FW_UPLOAD_ERR_CANCELED]     = FPGA_IMAGE_ERR_CANCELED,
+	[FW_UPLOAD_ERR_BUSY]	     = FPGA_IMAGE_ERR_BUSY,
+	[FW_UPLOAD_ERR_INVALID_SIZE] = FPGA_IMAGE_ERR_INVALID_SIZE,
+	[FW_UPLOAD_ERR_RW_ERROR]     = FPGA_IMAGE_ERR_RW_ERROR,
+	[FW_UPLOAD_ERR_WEAROUT]	     = FPGA_IMAGE_ERR_WEAROUT,
+	[FW_UPLOAD_ERR_MAX]	     = FPGA_IMAGE_ERR_MAX
+};
+
+static const int fw_upload_progress(struct device *dev, enum fw_upload_prog prog)
+{
+	int status = FPGA_IMAGE_PROG_MAX;
+
+	if (prog < FW_UPLOAD_PROG_MAX)
+		status = fw_upload_prog_code[prog];
+	else
+		dev_err(dev, "Invalid status during secure update: %d\n", prog);
+
+	return status;
+}
+
+static const int fw_upload_error(struct device *dev, enum fw_upload_err err_code)
+{
+	int error = FPGA_IMAGE_ERR_MAX;
+
+	if (err_code < FW_UPLOAD_ERR_MAX)
+		error = fw_upload_err_code[err_code];
+	else
+		dev_err(dev, "Invalid error code during secure update: %d\n",
+			err_code);
+
+	return error;
+}
+
+static int fpga_image_load_ioctl_status(struct fw_upload_priv *fwlp,
 					unsigned long arg)
 {
 	struct fpga_image_status status;
+	struct fpga_image_load *imgld;
+	struct fw_upload *fwl;
+	struct device *dev;
+
+	fwl = fwlp->fw_upload;
+	imgld = (struct fpga_image_load *)fwl->priv;
+	dev = &imgld->dev;
 
 	memset(&status, 0, sizeof(status));
-	status.progress = imgld->progress;
-	status.remaining_size = imgld->remaining_size;
-	status.err_progress = imgld->err_progress;
-	status.err_code = imgld->err_code;
+	status.progress = fw_upload_progress(dev, fwlp->progress);
+	status.remaining_size = fwlp->remaining_size;
+	status.err_progress = fw_upload_progress(dev, fwlp->err_progress);
+	status.err_code = fw_upload_error(dev, fwlp->err_code);
 
 	if (copy_to_user((void __user *)arg, &status, sizeof(status)))
 		return -EFAULT;
@@ -186,13 +279,13 @@ static int fpga_image_load_ioctl_status(struct fpga_image_load *imgld,
 	return 0;
 }
 
-static int fpga_image_load_ioctl_cancel(struct fpga_image_load *imgld,
+static int fpga_image_load_ioctl_cancel(struct fw_upload_priv *fwlp,
 					unsigned long arg)
 {
-	if (imgld->progress == FPGA_IMAGE_PROG_IDLE)
+	if (fwlp->progress == FW_UPLOAD_PROG_IDLE)
 		return -ENODEV;
 
-	imgld->ops->cancel(imgld);
+	fwlp->ops->cancel(fwlp->fw_upload);
 	return 0;
 }
 
@@ -200,26 +293,29 @@ static long fpga_image_load_ioctl(struct file *filp, unsigned int cmd,
 				  unsigned long arg)
 {
 	struct fpga_image_load *imgld = filp->private_data;
+	struct fw_upload_priv *fwlp;
 	int ret = 0;
 
-	mutex_lock(&imgld->lock);
+	fwlp = imgld->fw_upload_priv;
+
+	mutex_lock(&fwlp->lock);
 
 	switch (cmd) {
 	case FPGA_IMAGE_LOAD_WRITE:
-		ret = fpga_image_load_ioctl_write(imgld, arg);
+		ret = fpga_image_load_ioctl_write(fwlp, arg);
 		break;
 	case FPGA_IMAGE_LOAD_STATUS:
-		ret = fpga_image_load_ioctl_status(imgld, arg);
+		ret = fpga_image_load_ioctl_status(fwlp, arg);
 		break;
 	case FPGA_IMAGE_LOAD_CANCEL:
-		ret = fpga_image_load_ioctl_cancel(imgld, arg);
+		ret = fpga_image_load_ioctl_cancel(fwlp, arg);
 		break;
 	default:
 		ret = -ENOTTY;
 		break;
 	}
 
-	mutex_unlock(&imgld->lock);
+	mutex_unlock(&fwlp->lock);
 
 	return ret;
 }
@@ -240,17 +336,20 @@ static int fpga_image_load_open(struct inode *inode, struct file *filp)
 static int fpga_image_load_release(struct inode *inode, struct file *filp)
 {
 	struct fpga_image_load *imgld = filp->private_data;
+	struct fw_upload_priv *fwlp;
 
-	mutex_lock(&imgld->lock);
-	if (imgld->progress == FPGA_IMAGE_PROG_IDLE) {
-		mutex_unlock(&imgld->lock);
+	fwlp = imgld->fw_upload_priv;
+
+	mutex_lock(&fwlp->lock);
+	if (fwlp->progress == FW_UPLOAD_PROG_IDLE) {
+		mutex_unlock(&fwlp->lock);
 		goto close_exit;
 	}
 
-	imgld->ops->cancel(imgld);
+	fwlp->ops->cancel(fwlp->fw_upload);
 
-	mutex_unlock(&imgld->lock);
-	flush_work(&imgld->work);
+	mutex_unlock(&fwlp->lock);
+	flush_work(&fwlp->work);
 
 close_exit:
 	atomic_set(&imgld->opened, 0);
@@ -268,18 +367,21 @@ static const struct file_operations fpga_image_load_fops = {
 /**
  * fpga_image_load_register - create and register an FPGA Image Load Device
  *
+ * @module: kernel module of this device
  * @parent: fpga image load device from pdev
+ * @name: firmware name - unused by image load driver
  * @ops:   pointer to a structure of image load callback functions
- * @priv:   fpga image load private data
+ * @dd_handle: pointer to the lower-level device driver
  *
- * Returns a struct fpga_image_load pointer on success, or ERR_PTR() on
- * error. The caller of this function is responsible for calling
- * fpga_image_load_unregister().
+ * Return: struct fw_upload pointer or ERR_PTR()
  */
-struct fpga_image_load *
-fpga_image_load_register(struct device *parent,
-			 const struct fpga_image_load_ops *ops, void *priv)
+struct fw_upload *
+fpga_image_load_register(struct module *module, struct device *parent,
+			 const char *name, const struct fw_upload_ops *ops,
+			 void *dd_handle)
 {
+	struct fw_upload_priv *fw_upload_priv;
+	struct fw_upload *fw_upload;
 	struct fpga_image_load *imgld;
 	int ret;
 
@@ -289,26 +391,48 @@ fpga_image_load_register(struct device *parent,
 		return ERR_PTR(-ENOMEM);
 	}
 
+	if (!try_module_get(module))
+		return ERR_PTR(-EFAULT);
+
+	fw_upload = kzalloc(sizeof(*fw_upload), GFP_KERNEL);
+	if (!fw_upload) {
+		ret = -ENOMEM;
+		goto exit_module_put;
+	}
+
+	fw_upload_priv = kzalloc(sizeof(*fw_upload_priv), GFP_KERNEL);
+	if (!fw_upload_priv) {
+		ret = -ENOMEM;
+		goto free_fw_upload;
+	}
+
+	fw_upload_priv->fw_upload = fw_upload;
+	fw_upload_priv->ops = ops;
+	mutex_init(&fw_upload_priv->lock);
+	fw_upload_priv->module = module;
+	fw_upload_priv->name = name;
+	fw_upload_priv->err_code = 0;
+	fw_upload_priv->progress = FW_UPLOAD_PROG_IDLE;
+	fw_upload_priv->finished = NULL;
+	INIT_WORK(&fw_upload_priv->work, fw_upload_main);
+	fw_upload->dd_handle = dd_handle;
+
 	imgld = kzalloc(sizeof(*imgld), GFP_KERNEL);
-	if (!imgld)
-		return ERR_PTR(-ENOMEM);
+	if (!imgld) {
+		ret = -ENOMEM;
+		goto free_fw_upload_priv;
+	}
 
-	ret = xa_alloc(&fpga_image_load_xa, &imgld->dev.id, imgld, IMAGE_LOAD_XA_LIMIT,
-		       GFP_KERNEL);
-	if (ret)
-		goto error_kfree;
-
-	mutex_init(&imgld->lock);
-
-	imgld->priv = priv;
-	imgld->ops = ops;
-	imgld->err_code = 0;
-	imgld->progress = FPGA_IMAGE_PROG_IDLE;
-	INIT_WORK(&imgld->work, fpga_image_do_load);
-
-	imgld->dev.class = fpga_image_load_class;
+	fw_upload->priv = imgld;
+	imgld->fw_upload_priv = fw_upload_priv;
 	imgld->dev.parent = parent;
+	imgld->dev.class = fpga_image_load_class;
 	imgld->dev.devt = MKDEV(MAJOR(fpga_image_devt), imgld->dev.id);
+
+	ret = xa_alloc(&fpga_image_load_xa, &imgld->dev.id, imgld,
+		       IMAGE_LOAD_XA_LIMIT, GFP_KERNEL);
+	if (ret)
+		goto free_imgld;
 
 	ret = dev_set_name(&imgld->dev, "fpga_image_load%d", imgld->dev.id);
 	if (ret) {
@@ -320,7 +444,7 @@ fpga_image_load_register(struct device *parent,
 	ret = device_register(&imgld->dev);
 	if (ret) {
 		put_device(&imgld->dev);
-		return ERR_PTR(ret);
+		goto exit_module_put;
 	}
 
 	cdev_init(&imgld->cdev, &fpga_image_load_fops);
@@ -330,16 +454,25 @@ fpga_image_load_register(struct device *parent,
 	ret = cdev_add(&imgld->cdev, imgld->dev.devt, 1);
 	if (ret) {
 		put_device(&imgld->dev);
-		return ERR_PTR(ret);
+		goto exit_module_put;
 	}
 
-	return imgld;
+	return fw_upload;
 
 error_device:
 	xa_erase(&fpga_image_load_xa, imgld->dev.id);
 
-error_kfree:
+free_imgld:
 	kfree(imgld);
+
+free_fw_upload_priv:
+	kfree(fw_upload_priv);
+
+free_fw_upload:
+	kfree(fw_upload);
+
+exit_module_put:
+	module_put(module);
 
 	return ERR_PTR(ret);
 }
@@ -348,37 +481,49 @@ EXPORT_SYMBOL_GPL(fpga_image_load_register);
 /**
  * fpga_image_load_unregister - unregister an FPGA image load device
  *
- * @imgld: pointer to struct fpga_image_load
+ * @fw_upload: pointer to struct fw_upload
  *
  * This function is intended for use in the parent driver's remove()
- * function. The driver_unload flag prevents new updates from starting
- * once the unregister process has begun.
+ * function.
  */
-void fpga_image_load_unregister(struct fpga_image_load *imgld)
+void fpga_image_load_unregister(struct fw_upload *fw_upload)
 {
-	mutex_lock(&imgld->lock);
-	imgld->driver_unload = true;
-	if (imgld->progress == FPGA_IMAGE_PROG_IDLE) {
-		mutex_unlock(&imgld->lock);
+	struct fpga_image_load *imgld = fw_upload->priv;
+	struct fw_upload_priv *fw_upload_priv;
+	struct module *module;
+
+	fw_upload_priv = imgld->fw_upload_priv;
+	module = fw_upload_priv->module;
+
+	mutex_lock(&fw_upload_priv->lock);
+	if (fw_upload_priv->progress == FW_UPLOAD_PROG_IDLE) {
+		mutex_unlock(&fw_upload_priv->lock);
 		goto unregister;
 	}
 
-	imgld->ops->cancel(imgld);
+	fw_upload_priv->ops->cancel(fw_upload);
+	mutex_unlock(&fw_upload_priv->lock);
 
-	mutex_unlock(&imgld->lock);
-	flush_work(&imgld->work);
+	/* Ensure lower-level device-driver is finished */
+	flush_work(&fw_upload_priv->work);
 
 unregister:
 	cdev_del(&imgld->cdev);
 	device_unregister(&imgld->dev);
+	module_put(module);
 }
 EXPORT_SYMBOL_GPL(fpga_image_load_unregister);
 
 static void fpga_image_load_dev_release(struct device *dev)
 {
 	struct fpga_image_load *imgld = to_image_load(dev);
+	struct fw_upload_priv *fwlp;
+
+	fwlp = imgld->fw_upload_priv;
 
 	xa_erase(&fpga_image_load_xa, imgld->dev.id);
+	kfree(fwlp->fw_upload);
+	kfree(fwlp);
 	kfree(imgld);
 }
 
--- include/linux/fpga/fpga-image-load.h
+++ include/linux/fpga/fpga-image-load.h
@@ -7,63 +7,74 @@
 #ifndef _LINUX_FPGA_IMAGE_LOAD_H
 #define _LINUX_FPGA_IMAGE_LOAD_H
 
-#include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/eventfd.h>
 #include <linux/mutex.h>
 #include <linux/types.h>
 #include <uapi/linux/fpga-image-load.h>
 
-struct fpga_image_load;
+/**
+ * enum fw_upload_err - firmware upload error codes
+ * @FW_UPLOAD_ERR_NONE: returned to indicate success
+ * @FW_UPLOAD_ERR_HW_ERROR: error signalled by hardware, see kernel log
+ * @FW_UPLOAD_ERR_TIMEOUT: SW timed out on handshake with HW/firmware
+ * @FW_UPLOAD_ERR_CANCELED: upload was cancelled by the user
+ * @FW_UPLOAD_ERR_BUSY: there is an upload operation already in progress
+ * @FW_UPLOAD_ERR_INVALID_SIZE: invalid firmware image size
+ * @FW_UPLOAD_ERR_RW_ERROR: read or write to HW failed, see kernel log
+ * @FW_UPLOAD_ERR_WEAROUT: FLASH device is approaching wear-out, wait & retry
+ * @FW_UPLOAD_ERR_MAX: Maximum error code marker
+ */
+enum fw_upload_err {
+	FW_UPLOAD_ERR_NONE,
+	FW_UPLOAD_ERR_HW_ERROR,
+	FW_UPLOAD_ERR_TIMEOUT,
+	FW_UPLOAD_ERR_CANCELED,
+	FW_UPLOAD_ERR_BUSY,
+	FW_UPLOAD_ERR_INVALID_SIZE,
+	FW_UPLOAD_ERR_RW_ERROR,
+	FW_UPLOAD_ERR_WEAROUT,
+	FW_UPLOAD_ERR_MAX
+};
+
+struct fw_upload {
+	void *dd_handle; /* reference to parent driver */
+	void *priv;	 /* firmware loader private fields */
+};
 
 /**
- * struct fpga_image_load_ops - device specific operations
- * @prepare:		    Required: Prepare secure update
- * @write:		    Required: The write() op receives the remaining
- *			    size to be written and must return the actual
- *			    size written or a negative error code. The write()
- *			    op will be called repeatedly until all data is
- *			    written.
- * @poll_complete:	    Required: Check for the completion of the
- *			    HW authentication/programming process.
- * @cancel:		    Required: Request cancellation of update. This op
- *			    is called from the context of a different kernel
- *			    thread, so race conditions need to be considered.
- * @cleanup:		    Optional: Complements the prepare()
- *			    function and is called at the completion
- *			    of the update, whether success or failure,
- *			    if the prepare function succeeded.
+ * struct fw_upload_ops - device specific operations to support firmware upload
+ * @prepare:		  Required: Prepare secure update
+ * @write:		  Required: The write() op receives the remaining
+ *			  size to be written and must return the actual
+ *			  size written or a negative error code. The write()
+ *			  op will be called repeatedly until all data is
+ *			  written.
+ * @poll_complete:	  Required: Check for the completion of the
+ *			  HW authentication/programming process.
+ * @cancel:		  Required: Request cancellation of update. This op
+ *			  is called from the context of a different kernel
+ *			  thread, so race conditions need to be considered.
+ * @cleanup:		  Optional: Complements the prepare()
+ *			  function and is called at the completion
+ *			  of the update, on success or failure, if the
+ *			  prepare function succeeded.
  */
-struct fpga_image_load_ops {
-	u32 (*prepare)(struct fpga_image_load *imgld, const u8 *data, u32 size);
-	s32 (*write)(struct fpga_image_load *imgld, const u8 *data,
-		     u32 offset, u32 size);
-	u32 (*poll_complete)(struct fpga_image_load *imgld);
-	void (*cancel)(struct fpga_image_load *imgld);
-	void (*cleanup)(struct fpga_image_load *imgld);
+struct fw_upload_ops {
+	enum fw_upload_err (*prepare)(struct fw_upload *fw_upload,
+				      const u8 *data, u32 size);
+	enum fw_upload_err (*write)(struct fw_upload *fw_upload,
+				    const u8 *data, u32 offset,
+				    u32 size, u32 *written);
+	enum fw_upload_err (*poll_complete)(struct fw_upload *fw_upload);
+	void (*cancel)(struct fw_upload *fw_upload);
+	void (*cleanup)(struct fw_upload *fw_upload);
 };
 
-struct fpga_image_load {
-	struct device dev;
-	struct cdev cdev;
-	const struct fpga_image_load_ops *ops;
-	struct mutex lock;		/* protect data structure contents */
-	atomic_t opened;
-	struct work_struct work;
-	const u8 *data;			/* pointer to update data */
-	u32 remaining_size;		/* size remaining to transfer */
-	u32 progress;
-	u32 err_progress;		/* progress at time of error */
-	u32 err_code;			/* image load error code */
-	bool driver_unload;
-	struct eventfd_ctx *finished;
-	void *priv;
-};
-
-struct fpga_image_load *
-fpga_image_load_register(struct device *dev,
-			 const struct fpga_image_load_ops *ops, void *priv);
-
-void fpga_image_load_unregister(struct fpga_image_load *imgld);
+struct fw_upload *
+fpga_image_load_register(struct module *module, struct device *parent,
+			 const char *name, const struct fw_upload_ops *ops,
+			 void *dd_handle);
+void fpga_image_load_unregister(struct fw_upload *fw_upload);
 
 #endif
