e65609eec1ddbb56e1453e8de0b848939f246a63
Russ Weight <russell.h.weight@intel.com>
2023-01-30 16:50:16 -0800

    mfd: intel-m10-bmc: Sync BMC implementation with upstream code
    The BMC driver changes have been accepted upstream to split the BMC driver
    into core and SPI parts and to add PMCI+N6000 support. The final version
    of the code included significant changes:
    
    o Instead of tracking the device type and using conditional statement,
      we are taking advantage of the driver_data element of the device
      structure to manage device-specific data and functions, including
      the different addresses for many of the BMC registers.
    o An m10bmc_sec_ops structure is used to track device-specific
      differences in the secure update driver.
    o Many of the BMC related constant names have been modified.
    o New locking is added to manage the MUX for burst reads and writes
      to FLASH for PMCI systems.
    
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>
    (cherry picked from commit 1ed4dedf6440ae4d1eff83e08d6b4bbbd5f3eebb)

---
+++ include/linux/minmax.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __BACKPORT_MINMAX_H
+#define __BACKPORT_MINMAX_H
+
+#include <linux/version.h>
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0) && RHEL_RELEASE_CODE <= 0x804
+#include <linux/kernel.h>
+#else
+#include_next <linux/minmax.h>
+#endif
+
+#endif
