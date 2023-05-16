9140f2fe03c20e006b0ed7b2870400cad0bbf64e
Muralix <muralix.narayanan@intel.com>
2023-05-16 12:24:39 +0800

    backport: Add missing macros: GIGA, MEGA, FIELD_MAX
    Add header files to the backport driver to provide missing macros for
    older kernels.
    
    Signed-off-by: Murali Narayanan <muralix.narayanan@intel.com>

---
+++ include/linux/units.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __BACKPORT_UNITS_H
+#define __BACKPORT_UNITS_H
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0) && RHEL_RELEASE_CODE <= 0x902
+#define GIGA    1000000000UL
+#define MEGA    1000000UL
+#define MICRO   1000000UL
+#else
+#include_next <linux/units.h>
+#endif
+
+#endif
