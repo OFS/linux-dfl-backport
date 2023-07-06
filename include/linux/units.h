a09a20d7d6a040908b1200b090d278d919762f99
Russ Weight <russell.h.weight@intel.com>
2023-07-06 16:49:47 -0700

    backport: Add definition for MILLI to units.h
    Add definition for MILLI to the local linux/units.h file. Also replaced
    alignment spaces with tabs for the unit macro definitions in this file.
    
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>

--- include/linux/units.h
+++ include/linux/units.h
@@ -5,9 +5,10 @@
 #include <linux/version.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0) && RHEL_RELEASE_CODE <= 0x902
-#define GIGA    1000000000UL
-#define MEGA    1000000UL
-#define MICRO   1000000UL
+#define GIGA	1000000000UL
+#define MEGA	1000000UL
+#define MICRO	1000000UL
+#define MILLI	1000UL
 #else
 #include_next <linux/units.h>
 #endif
