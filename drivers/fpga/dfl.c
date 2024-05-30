e2b6058e3846efee2ec2422f327575e4fcc991ee
Peter Colberg <peter.colberg@intel.com>
2024-05-30 16:54:00 -0400

    backport: pass const struct device * to dfl_bus_uevent() on RHEL 9.4
    Fixes: 11d73695e5de ("backport: pass const struct device * to dfl_bus_uevent()")
    Signed-off-by: Peter Colberg <peter.colberg@intel.com>

--- drivers/fpga/dfl.c
+++ drivers/fpga/dfl.c
@@ -300,13 +300,13 @@ static void dfl_bus_remove(struct device *dev)
 
 #define DFL_ALIAS_BUF_LEN 64
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0) && RHEL_RELEASE_CODE < 0x904
 static int dfl_bus_uevent(struct device *dev, struct kobj_uevent_env *env)
 #else
 static int dfl_bus_uevent(const struct device *dev, struct kobj_uevent_env *env)
 #endif
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0) && RHEL_RELEASE_CODE < 0x904
 	struct dfl_device *ddev = to_dfl_dev(dev);
 #else
 	const struct dfl_device *ddev = to_dfl_dev(dev);
