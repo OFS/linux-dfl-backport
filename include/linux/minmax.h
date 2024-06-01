db0ecfe7626e334fa7f388b0fe5976a9070b0e27
Peter Colberg <peter.colberg@intel.com>
2024-05-31 20:10:34 -0400

    backport: add in_range() compatibility macro
    in_range() was added in commit f9bff0e31881 ("minmax: add in_range() macro").

--- include/linux/minmax.h
+++ include/linux/minmax.h
@@ -11,4 +11,20 @@
 #include_next <linux/minmax.h>
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0)
+static inline bool in_range64(u64 val, u64 start, u64 len)
+{
+	return (val - start) < len;
+}
+
+static inline bool in_range32(u32 val, u32 start, u32 len)
+{
+	return (val - start) < len;
+}
+
+#define in_range(val, start, len)					\
+	((sizeof(start) | sizeof(len) | sizeof(val)) <= sizeof(u32) ?	\
+		in_range32(val, start, len) : in_range64(val, start, len))
+#endif
+
 #endif
